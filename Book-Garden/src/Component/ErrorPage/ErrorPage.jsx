12a032aecf80a2988ff7e746d331203b13a44966
Shahriyar <chyshahriyar@gmail.com>
2024-03-26 22:52:31 +0600

    Error page handle

--- Book-Garden/src/Component/ErrorPage/ErrorPage.jsx
+++ Book-Garden/src/Component/ErrorPage/ErrorPage.jsx
@@ -2,8 +2,9 @@
 
 const ErrorPage = () => {
     return (
-        <div>
-            <h2>Ooppss !!</h2>
+        <div className="mt-60">
+            <h2 className="text-xl mb-16 text-center">The page you are looking for can not be found.</h2>
+            <h1 className="text-9xl font-extrabold text-center">404</h1>
         </div>
     );
 };
--- Book-Garden/src/Component/Home/Home.jsx
+++ Book-Garden/src/Component/Home/Home.jsx
@@ -1,9 +1,11 @@
+import Books from "../Books/Books";
 
 
 const Home = () => {
     return (
         <div>
-            <div>
+            {/* header part */}
+            <div className="mb-16">
                 <div className=" bg-[#c9c6c64f] rounded-3xl py-8">
                     <div className="hero-content flex-col lg:flex-row-reverse lg:w-10/12 mx-auto">
                         <img src="https://i.ibb.co/HpwdqKD/Coffee-Table-Book-Gifts-for-Christmas-2019-Eco-Friendly-Mom-Blog-Sustainable-Family-Influencer-2022.jpg" className="w-72 h-3/4 rounded-lg shadow-2xl" />
@@ -13,6 +15,10 @@ const Home = () => {
                         </div>
                     </div>
                 </div>
+            </div>
+            <div>
+                <h1 className="text-4xl font-bold text-center mb-10">Books</h1>
+                <Books></Books>
             </div>
         </div>
     );
